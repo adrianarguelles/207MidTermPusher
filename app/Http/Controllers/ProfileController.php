5a0379e01e6b83b1d7621a5394e73c4f5562bf67
Jasper Francisco <jasper.francisco@gmail.com>
2022-04-18 10:22:43 +0000

    Add validation logic

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 
 /**
@@ -18,12 +19,17 @@ class ProfileController extends Controller
     // Endpoint to update a certain user's name and profile picture
     public function update(Request $request, $id)
     {
-        // TODO: Validate input before saving changes to the database
-        // $request->validate([
-        //     'firstname' => 'required',
-        //     'lastname' => 'required',
-        //     'profilepicture' => 'image|mimes:jpeg,jpg,png|max:2048'
-        // ]);
+        // Validate input before saving changes to the database
+        $validator = Validator::make($request->all(), [
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'profilepicture' => 'image|mimes:jpeg,jpg,png|max:4096'
+        ]);
+        
+        // If the request does not pass validation, inform the user
+        if ($validator->fails()) {
+            return response($validator->errors(), 400); 
+        }
 
         // Retrieve input data from the request
         $firstName = $request->input('firstname');
@@ -34,16 +40,17 @@ class ProfileController extends Controller
         $user = User::findOrFail($id);
         $user->first_name = $firstName;
         $user->last_name = $lastName;
-        // $user->profile_picture = $profilePicture;
+        $user->profile_picture = $profilePicture;
+        // TODO: Allow updating of profile picture
 
         $user->save();
         
-        // Respond successfully
+        // Echo back the request as a successful response
         return [
             "id" => $id,
             "firstName" => $user->first_name,
             "lastName" => $user->last_name,
-            "profilePictureExtension" => (!is_null($profilePicture)) ? $profilePicture->extension() : ""
+            "profilePicture" => (!is_null($profilePicture)) ? $profilePicture->extension() : ""
         ];
     }
 
