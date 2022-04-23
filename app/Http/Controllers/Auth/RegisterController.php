b0eaac81bdeb2fc5f2b8a7a15a76ce4be668b141
allegoricali <ali.cunanan@gmail.com>
2022-04-23 20:25:08 +0800

    Remove the profile pic size limit
    Updated the Register Controller to remove the limit on file size of profile pic during registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -58,7 +58,7 @@ class RegisterController extends Controller
             'last_name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'profile_picture' => ['image', 'size:4096']
+            'profile_picture' => ['image']
         ]);
     }
 
